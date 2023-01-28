6c3c01e306ae212ec7d2cec4c987f73c5c609be8
Giulio de Cadilhac <g.decadilhac@gmail.com>
2023-01-28 14:47:43 +0000

    seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+import 'json'
+import 'open-uri'
+
+json_string = URI.open('https://tmdb.lewagon.com/movie/top_rated').read
+json_hash = JSON.parse(json_string)
+
+puts 'Deleting database contents, before seeding.'
+Movie.destroy_all
+
+json_hash['results'].each do |movie|
+  Movie.create(
+    title: movie['original_title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/original#{movie['backdrop_path']}",
+    rating: movie['vote_average']
+  )
+end
